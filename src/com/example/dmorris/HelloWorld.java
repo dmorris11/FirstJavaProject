44303ecc870b986097b3482d7cfcdf7d1a0b25eb
Daven Morris <davenmorris11@gmail.com>
2018-08-12 21:35:51 -0500

    added dialog

--- src/com/example/dmorris/HelloWorld.java
+++ src/com/example/dmorris/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.example.dmorris;
 
+import javax.swing.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null, "Welcome to IntelliJ");
     }
 }
